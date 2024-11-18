ee71779d9a079231f97e00b81382ac0241f06a81
Subhrajit Sanyal <subhrajitsanyal02@gmail.com>
2024-11-19 01:07:12 +0530

    Initial commit

---
+++ Frontend/src/Pages/Doctors/DoctorDetails.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const DoctorsDetails = () => {
+  return (
+    <div>
+      DoctorsDetails
+    </div>
+  )
+}
+
+export default DoctorsDetails
---
+++ Frontend/src/Pages/Doctors/Doctors.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const Doctors = () => {
+  return (
+    <div>
+      Doctors
+    </div>
+  )
+}
+
+export default Doctors
