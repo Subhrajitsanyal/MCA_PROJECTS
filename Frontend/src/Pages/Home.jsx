3844de27e083eeb968aaed728c5e0a23def55cbe
Subhrajit Sanyal <subhrajitsanyal02@gmail.com>
2024-11-19 23:58:59 +0530

    made some changes regarding the header file and fixed some bugs

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,13 +1,25 @@
-
-
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      {/* ============ hero section =============== */}
+      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* ======= Hero section content ================ */}
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[45px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                  We help patients live a healthy, longer life
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
